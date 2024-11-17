e2e85d639e2fad1763a3554061fa67be0132661b
Huynh <socialnetworksite15@gmail.com>
2024-11-17 08:20:56 +0700

    use forwardRef in uncontrolled component case

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,22 +1,39 @@
+import { forwardRef } from "react";
+
 type Props = {
   label: string;
-  value: string;
+  value?: string;
   inputKey: "username" | "password";
-  onChange: (inputKey: string, e: any) => void;
+  onChange?: (inputKey: string, e: any) => void;
   type?: string;
+  defaultValue?: string;
 };
 
-const Input = ({ label, value, inputKey, onChange, type = "text" }: Props) => {
-  return (
-    <div>
-      <label>{label}</label>
-      <input
-        type={type}
-        value={value}
-        onChange={(e) => onChange(inputKey, e)}
-      />
-    </div>
-  );
-};
+const Input = forwardRef<HTMLInputElement, Props>(
+  (
+    {
+      label,
+      value,
+      inputKey,
+      onChange = () => {},
+      type = "text",
+      defaultValue,
+    }: Props,
+    ref
+  ) => {
+    return (
+      <div>
+        <label>{label}</label>
+        <input
+          type={type}
+          ref={ref}
+          defaultValue={defaultValue}
+          value={value}
+          onChange={(e) => onChange(inputKey, e)}
+        />
+      </div>
+    );
+  }
+);
 
 export default Input;
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,23 +1,30 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Input from "../components/Input";
 
 const Login = () => {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: "",
-  });
+  const usernameRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+  // const [formData, setFormData] = useState({
+  //   username: "",
+  //   password: "",
+  // });
 
-  const onHandleChange = (type: string, e: any) => {
-    setFormData({
-      ...formData,
-      [type]: e.target.value,
-    });
-  };
+  // const onHandleChange = (type: string, e: any) => {
+  //   setFormData({
+  //     ...formData,
+  //     [type]: e.target.value,
+  //   });
+  // };
 
   const handleSubmit = (e: any) => {
     e.preventDefault();
-    console.log(formData.username);
-    console.log(formData.password);
+    if (usernameRef.current) {
+      const username = usernameRef.current.value;
+      console.log(username);
+    }
+    if (passwordRef.current) {
+      console.log(passwordRef.current.value);
+    }
   };
 
   console.log("render form");
@@ -26,15 +33,19 @@ const Login = () => {
       <Input
         inputKey="username"
         label="Username"
-        value={formData.username}
-        onChange={onHandleChange}
+        // value={formData.username}
+        // onChange={onHandleChange}
+        ref={usernameRef}
+        defaultValue={""}
       />
       <Input
         inputKey="password"
         label="Password"
-        value={formData.password}
-        onChange={onHandleChange}
+        // value={formData.password}
+        // onChange={onHandleChange}
         type="password"
+        ref={passwordRef}
+        defaultValue={""}
       />
       <button type="submit">Submit</button>
     </form>
