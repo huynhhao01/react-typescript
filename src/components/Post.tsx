66f8d87c5b5598af38ad2ef96c39ce5f37a6faee
Huynh <socialnetworksite15@gmail.com>
2024-11-24 08:29:42 +0700

    use onChange to can type value in input

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -7,16 +7,21 @@ interface Props {
 
 const Post = ({ post }: Props) => {
   const [editingTitle, setEditingTitle] = useState(false);
+  const [titleText, setTitleText] = useState(post.title);
 
   const showInput = (value: boolean) => {
     setEditingTitle(value);
+  };
+
+  const handleChange = (e: any) => {
+    setTitleText(e.target.value);
   };
 
   return (
     <div>
       {editingTitle ? (
         <div>
-          <input type="text" value={post.title} />
+          <input type="text" value={titleText} onChange={handleChange} />
           <button onClick={() => showInput(false)}>Save</button>
         </div>
       ) : (
